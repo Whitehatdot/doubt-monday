629f3e3420b53e6abc42ba82faaf9e4bc475f1a0
Whitehatdot <93816623+Whitehatdot@users.noreply.github.com>
2022-08-19 18:55:01 +0530

    Add files via upload

---
+++ twomonday.js
@@ -0,0 +1,45 @@
+one = localStorage.getItem("seventoeightmonday");
+console.log(one);
+document.getElementById("friend").innerHTML = one;
+
+two = localStorage.getItem("eighttoninemonday");
+document.getElementById("two").innerHTML = "8 to 9  = " + two  + ".";
+
+three = localStorage.getItem("ninetotenmonday");
+document.getElementById("three").innerHTML = "9 to 10  = " + three  + ".";
+
+four = localStorage.getItem("tentoelevenmonday");
+document.getElementById("four").innerHTML = "10 to 11  = " + four  + ".";
+
+five = localStorage.getItem("eleventotwelvemonday");
+document.getElementById("five").innerHTML = "11 to 12  = " + five  + ".";
+
+six = localStorage.getItem("twelvetoonemonday");
+document.getElementById("six").innerHTML = "12 to 1 = " + six  + ".";
+
+seven = localStorage.getItem("onetotwomonday");
+document.getElementById("seven").innerHTML = "1 to 2  = " + seven  + ".";
+
+eight = localStorage.getItem("twotothreemonday");
+document.getElementById("eight").innerHTML = "2 to 3  = " + eight  + ".";
+
+nine = localStorage.getItem("threetofourmonday");
+document.getElementById("nine").innerHTML = "3 to 4  = " + nine  + ".";
+
+ten = localStorage.getItem("fourtofivemonday");
+document.getElementById("ten").innerHTML = "4 to 5  = " + ten  + ".";
+
+eleven = localStorage.getItem("fivetosixmonday");
+document.getElementById("eleven").innerHTML = "5 to 6  = " + eleven  + ".";
+
+twelve = localStorage.getItem("sixtosevenmonday");
+document.getElementById("twelve").innerHTML = "6 to 7  = " + twelve  + ".";
+
+thirteen = localStorage.getItem("sevenpmtoeightpmmonday");
+document.getElementById("thirteen").innerHTML = "7pm to 8pm  = " + thirteen  + ".";
+
+fourteen = localStorage.getItem("eightpmtoninepmmonday");
+document.getElementById("fourteen").innerHTML = "8pm to 9pm = " + fourteen  + ".";
+
+fifteen = localStorage.getItem("ninepmtotenpmmonday");
+document.getElementById("fifteen").innerHTML = "9pm to 10pm  = " + fifteen  + ".";
